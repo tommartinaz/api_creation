fa88da6f9cc0f6d2b3950fafdba44f8bfa6239a8
Tom Martin <tom.martinaz@gmail.com>
2017-06-30 16:12:47 -0700

    added user instructions

--- routes/detailRoutes.js
+++ routes/detailRoutes.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var knex = require('../db/knex');
 
 router.get('/', (req,res) => {
-    knex.raw(`select b.title, a.first_name, a.last_name, g.genre, p.name publisher_name, b.cover_img 
+    knex.raw(`select b.id, b.title, a.first_name author_first_name, a.last_name author_last_name, g.genre, p.name publisher_name, b.cover_img 
         from books b 
         join authors a 
         on a.id = b.author_id
         join genres g
         on b.genre_id = g.id
         join publishers p
-        on b.publisher_id = p.id`)
+        on b.publisher_id = p.id order by b.id`)
         .then(data => {
             res.send(data.rows);
         });
