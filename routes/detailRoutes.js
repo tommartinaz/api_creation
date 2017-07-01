61deae90bbe8a1945a1f1a3d163550b994e4ebf4
Tom Martin <tom.martinaz@gmail.com>
2017-07-01 08:44:40 -0700

    updated to use knex shortcuts

--- routes/detailRoutes.js
+++ routes/detailRoutes.js
@@ -3,16 +3,22 @@ var router = express.Router();
 var knex = require('../db/knex');
 
 router.get('/', (req,res) => {
-    knex.raw(`select b.id, b.title, a.first_name author_first_name, a.last_name author_last_name, g.genre, p.name publisher_name, b.cover_img 
-        from books b 
-        join authors a 
-        on a.id = b.author_id
-        join genres g
-        on b.genre_id = g.id
-        join publishers p
-        on b.publisher_id = p.id order by b.id`)
+    // knex.raw(`select b.id, b.title, a.first_name author_first_name, a.last_name author_last_name, g.genre, p.name publisher_name, b.cover_img 
+    //     from books b 
+    //     join authors a 
+    //     on a.id = b.author_id
+    //     join genres g
+    //     on b.genre_id = g.id
+    //     join publishers p
+    //     on b.publisher_id = p.id order by b.id`)
+        knex('books as b')
+        .join('authors as a', 'a.id', '=', 'b.author_id')
+        .join('genres as g', 'b.genre_id', '=', 'g.id')
+        .join('publishers as p', 'b.publisher_id', '=', 'p.id')
+        .select('b.id', 'b.title', 'a.first_name as author_first_name', 'a.last_name as author_last_name', 'g.genre', 'p.name as publisher_name', 'b.cover_img')
+        .orderBy('b.id')
         .then(data => {
-            res.send(data.rows);
+            res.send(data);
         });
     
 });
