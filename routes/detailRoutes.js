1334caadb82212984ab46ed9d20f226b74f10aac
Tom Martin <tom@Toms-iMac.local>
2017-07-02 04:19:11 -0700

    added year_published to details page

--- routes/detailRoutes.js
+++ routes/detailRoutes.js
@@ -15,7 +15,7 @@ router.get('/', (req,res) => {
         .join('authors as a', 'a.id', '=', 'b.author_id')
         .join('genres as g', 'b.genre_id', '=', 'g.id')
         .join('publishers as p', 'b.publisher_id', '=', 'p.id')
-        .select('b.id', 'b.title', 'a.first_name as author_first_name', 'a.last_name as author_last_name', 'g.genre', 'p.name as publisher_name', 'b.cover_img')
+        .select('b.id', 'b.title', 'a.first_name as author_first_name', 'a.last_name as author_last_name', 'g.genre', 'p.name as publisher_name', 'b.year_published', 'b.cover_img')
         .orderBy('b.id')
         .then(data => {
             res.send(data);
