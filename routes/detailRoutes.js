34eea442cf3fd8e48d13e9f4503c63a89c52138e
Tom Martin <tom@Toms-MacBook-Pro.local>
2017-08-08 22:04:17 -0700

    cleaned up details

--- routes/detailRoutes.js
+++ routes/detailRoutes.js
@@ -3,14 +3,6 @@ var router = express.Router();
 var knex = require('../db/knex');
 
 router.get('/', (req,res) => {
-    // knex.raw(`select b.id, b.title, a.first_name author_first_name, a.last_name author_last_name, g.genre, p.name publisher_name, b.cover_img 
-    //     from books b 
-    //     join authors a 
-    //     on a.id = b.author_id
-    //     join genres g
-    //     on b.genre_id = g.id
-    //     join publishers p
-    //     on b.publisher_id = p.id order by b.id`)
         knex('books as b')
         .join('authors as a', 'a.id', '=', 'b.author_id')
         .join('genres as g', 'b.genre_id', '=', 'g.id')
